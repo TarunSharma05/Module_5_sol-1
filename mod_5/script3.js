1462fba767bbc658849028a722232b2b0b5674a0
Vipin Kumar <65479359+Vip2016-0@users.noreply.github.com>
2020-06-20 10:27:47 +0530

    Update script3.js

--- mod_5/script3.js
+++ mod_5/script3.js
@@ -68,6 +68,13 @@ var switchMenuToActive = function () {
     document.querySelector("#navMenuButton").className = classes;
   }
 };
+  function chooseRandomCategory (categories) {
+  // Choose a random index into the array (from 0 inclusively until array length (exclusively))
+  var randomArrayIndex = Math.floor(Math.random() * categories.length);
+
+  // return category object with that randomArrayIndex
+  return categories[randomArrayIndex];
+}
 
 // On page load (before images or CSS)
 document.addEventListener("DOMContentLoaded", function (event) {
